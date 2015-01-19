605bc83418116cc2509d24ef9aac77e100d79568
Ryan <RR.Schweitzer@gmail.com>
2015-01-18 21:32:51 -0700

    constructor almost finished

--- closureReview.js
+++ closureReview.js
@@ -9,8 +9,8 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function. 
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //code here
-
+var getCalling = callFriend();
+getCalling("435-215-9248");
 
 
 /*
@@ -20,3 +20,68 @@ Write a function that accepts a function as it's only argument and returns a new
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 
 */
+
+
+
+var outer = function(func2) {
+	counter = 0;
+	return function() {
+		if (counter > 0) {
+			return func2();
+			counter++;
+		}
+	}
+}
+var passIn = function() {
+	console.log("I\'m  a function");
+}
+
+var inner = outer(passIn);
+
+
+
+
+
+var outer = function(func2, nTimes) {
+	counter = 1;
+	return function() {
+		if (counter <= nTimes) {
+			func2();
+			counter++;
+		}
+		else {
+			return console.log('STAHHP');
+		}
+	}
+}
+var passIn = function() {
+	console.log("I\'m  a function");
+}
+
+var inner = outer(passIn, 2);
+
+
+
+inner();
+inner();
+inner();
+inner();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- constructorReview.js
+++ constructorReview.js
@@ -1,26 +1,38 @@
 //Create a Animal Constructor that has the following parameters. species, name, legs, color, food (which is an array of foods they can eat).
 
-  //code here
-
+var Animal = function(species, name, legs, color, food) {
+  this.species = species,
+  this.name = name,
+  this.legs = legs,
+  this.color = color,
+  this.food = food
+}
 
 //Now create a person function that creates an object and returns it (not in constructor form) that has the following parameters. name, age, height, gender
 
-  //code here
+var Person = function(obj) {
+  var obj = {
+    obj.name = name,
+    obj.age = age,
+    obj.height = height,
+    obj.gender = gender
+  }
+  return obj;
+}
 
 
 //Create a animal array and a person array.
 
-  //code here
-
+var animal = [];
+var person = [];
 
 //Create two instances of Animal and push those into your animal array
 
-  //code here
+var animal1 = new Animal("Dog", "Louie", 4, "Black", "meat");
+var animal1 = new Animal("Cat", "Marvin", 4, "Black", "rodents");
 
 
 //Create two instances of person and push those into your person array.
-
-  //code here
 
 
 //Now we want every instance of Animal to have a eat method. This method will choose a random item in that instances food array, then alert "(name) ' ate ' (whichever food was chosen)".
